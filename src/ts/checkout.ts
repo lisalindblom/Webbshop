95802e04655f6983e7523179bf3f92d1efe966ff
robinsahin88 <robin.sahin88@gmail.com>
2022-12-22 09:10:10 +0100

    uppdate

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,4 +1,4 @@
 import { displayPaymentForm, showCart } from "./functions/functions";
 import { selectedItems } from "./services.ts/data";
 showCart(selectedItems);
-//displayPaymentForm();
+displayPaymentForm();
--- src/ts/functions/functions.ts
+++ src/ts/functions/functions.ts
@@ -171,10 +171,13 @@ export function displayPaymentForm() {
   countryName.placeholder = "Land";
   paymentMessage.innerHTML = "Vargod välj betalningsätt:";
   visaRadio.type = "radio";
+  visaRadio.name = "pay";
   visaText.innerHTML = "Visa";
   masterRadio.type = "radio";
+  masterRadio.name = "pay";
   masterText.innerHTML = "Master Card";
   klarnaRadio.type = "radio";
+  klarnaRadio.name = "pay";
   klarnaText.innerHTML = "Klarna faktura";
   cardNo.placeholder = "kortnummer";
   cardDate.placeholder = "utgångsdatum";
