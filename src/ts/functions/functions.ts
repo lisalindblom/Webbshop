064e2a0234672d940c463e522e70993305adb132
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2022-12-22 11:14:13 +0100

    styling av filter och produktsida

--- src/ts/functions/functions.ts
+++ src/ts/functions/functions.ts
@@ -2,6 +2,7 @@
 
 import { Products } from "../models/models";
 import { products, selectedItems } from "../services.ts/data";
+import { products as filteredList } from "../services.ts/data";
 
 export const showProducts = (products: Products[]) => {
   let container = document.getElementById(
@@ -20,6 +21,9 @@ export const showProducts = (products: Products[]) => {
       handleCLick(products[i]);
     });
     bookContainer.classList.add(products[i].type);
+    /********************************************** */
+    bookContainer.classList.add("bookContainer");
+    /*********************************************** */
     title.innerHTML = products[i].title;
     img.src = products[i].img;
     type.innerHTML = products[i].type;
@@ -102,7 +106,7 @@ export const showCart = (selectedItems: Products[]) => {
     const cartBadge = document.querySelectorAll("#cartItems");
 
     let badgeNumber = selectedItems.length;
-    cartBadge[i].innerHTML = badgeNumber.toString();
+    //cartBadge[i].innerHTML = badgeNumber.toString();
   }
 };
 
@@ -237,4 +241,89 @@ export function displayPaymentForm() {
   payForm.appendChild(cardDate);
   payForm.appendChild(cardCVC);
   payForm.appendChild(submitButton);
+}
+
+export function filterProducts() {
+  let btnOne = document.getElementById("btnOne") as HTMLInputElement;
+  let btnTwo = document.getElementById("btnTwo") as HTMLInputElement;
+  let btnThree = document.getElementById("btnThree") as HTMLInputElement;
+  let btnFour = document.getElementById("btnFour") as HTMLInputElement;
+  let btnFive = document.getElementById("btnFive") as HTMLInputElement;
+
+  let selectedFilter: string = "";
+
+  btnOne.addEventListener("click", () => {
+    selectedFilter = "Alla";
+  });
+  btnTwo.addEventListener("click", () => {
+    selectedFilter = "Pocket";
+  });
+  btnThree.addEventListener("click", () => {
+    selectedFilter = "Häftad";
+  });
+  btnFour.addEventListener("click", () => {
+    selectedFilter = "E-bok";
+  });
+  btnFive.addEventListener("click", () => {
+    selectedFilter = "Inbunden";
+  });
+
+  let submit = document.getElementById("btnSubmit") as HTMLButtonElement;
+
+  submit.addEventListener("click", () => {
+    filterAlternatives(filteredList);
+  });
+
+  function filterAlternatives(products: Products[]) {
+    let filteredList = products.filter((book) => {
+      return book.type === selectedFilter;
+    });
+
+    if (selectedFilter === "Alla") {
+      console.log("alla");
+      let container = document.getElementById(
+        "productpageWrapper"
+      ) as HTMLDivElement;
+
+      container.innerHTML = "";
+      showProducts(products);
+    } else {
+      showFilteredProducts(filteredList);
+    }
+  }
+
+  const showFilteredProducts = (filteredList: Products[]) => {
+    let container = document.getElementById(
+      "productpageWrapper"
+    ) as HTMLDivElement;
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < filteredList.length; i++) {
+      let bookContainer = document.createElement("div");
+      let title = document.createElement("h3");
+      let img = document.createElement("img");
+      let type = document.createElement("p");
+      let price = document.createElement("p");
+      let button = document.createElement("button");
+      button.innerHTML = "Buy";
+      button.addEventListener("click", () => {
+        handleCLick(filteredList[i]);
+      });
+      bookContainer.classList.add(filteredList[i].type);
+      bookContainer.classList.add("bookContainer");
+
+      title.innerHTML = filteredList[i].title;
+      img.src = filteredList[i].img;
+      type.innerHTML = filteredList[i].type;
+      price.innerHTML = JSON.stringify(filteredList[i].price);
+
+      bookContainer.appendChild(title);
+      bookContainer.appendChild(img);
+      bookContainer.appendChild(type);
+      bookContainer.appendChild(price);
+      bookContainer.appendChild(button);
+      container.appendChild(bookContainer);
+    }
+  };
 }
--- src/ts/productpage.ts
+++ src/ts/productpage.ts
@@ -1,90 +1,11 @@
-import { handleCLick, showProducts } from "./functions/functions";
+import {
+  filterProducts,
+  handleCLick,
+  showProducts,
+} from "./functions/functions";
 import { Products } from "./models/models";
 
 import { products as filteredList } from "./services.ts/data";
 
 showProducts(filteredList);
-
-let btnOne = document.getElementById("btnOne") as HTMLInputElement;
-let btnTwo = document.getElementById("btnTwo") as HTMLInputElement;
-let btnThree = document.getElementById("btnThree") as HTMLInputElement;
-let btnFour = document.getElementById("btnFour") as HTMLInputElement;
-let btnFive = document.getElementById("btnFive") as HTMLInputElement;
-
-let selectedFilter: string = "";
-
-btnOne.addEventListener("click", () => {
-  selectedFilter = "Alla";
-});
-btnTwo.addEventListener("click", () => {
-  selectedFilter = "Pocket";
-});
-btnThree.addEventListener("click", () => {
-  selectedFilter = "Häftad";
-});
-btnFour.addEventListener("click", () => {
-  selectedFilter = "E-bok";
-});
-btnFive.addEventListener("click", () => {
-  selectedFilter = "Inbunden";
-});
-
-let filterForm = document.getElementById("filterForm") as HTMLInputElement;
-let submit = document.getElementById("btnSubmit") as HTMLButtonElement;
-
-submit.addEventListener("click", () => {
-  filterAlternatives(filteredList);
-});
-
-function filterAlternatives(products: Products[]) {
-  let filteredList = products.filter((book) => {
-    return book.type === selectedFilter;
-  });
-
-  console.log(selectedFilter);
-  console.log(filteredList);
-  if (selectedFilter === "Alla") {
-    console.log("alla");
-    let container = document.getElementById(
-      "productpageWrapper"
-    ) as HTMLDivElement;
-
-    container.innerHTML = "";
-    showProducts(products);
-  } else {
-    showFilteredProducts(filteredList);
-  }
-}
-
-const showFilteredProducts = (filteredList: Products[]) => {
-  let container = document.getElementById(
-    "productpageWrapper"
-  ) as HTMLDivElement;
-
-  container.innerHTML = "";
-
-  for (let i = 0; i < filteredList.length; i++) {
-    let bookContainer = document.createElement("div");
-    let title = document.createElement("h3");
-    let img = document.createElement("img");
-    let type = document.createElement("p");
-    let price = document.createElement("p");
-    let button = document.createElement("button");
-    button.innerHTML = "Buy";
-    button.addEventListener("click", () => {
-      handleCLick(filteredList[i]);
-    });
-    bookContainer.classList.add(filteredList[i].type);
-    title.innerHTML = filteredList[i].title;
-    img.src = filteredList[i].img;
-    type.innerHTML = filteredList[i].type;
-    price.innerHTML = JSON.stringify(filteredList[i].price);
-
-    bookContainer.appendChild(title);
-    bookContainer.appendChild(img);
-    bookContainer.appendChild(type);
-    bookContainer.appendChild(price);
-    bookContainer.appendChild(button);
-    container.appendChild(bookContainer);
-  }
-};
+filterProducts();
